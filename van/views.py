99abbeffbe71bd25494ea30936e71e7d9dbb0901
김상은 <whitesandfox@gmail.com>
2014-10-21 17:57:44 +0900

    asdasd

--- van/views.py
+++ van/views.py
@@ -5,5 +5,6 @@ def index(request):
 	return HttpResponse("hello world!!")
 
 def pull(request):
+	os.system('touch /website/van/van/aa.txt')
 	os.system('cd /website/van && sudo -u van git reset --hard HEAD && sudo -u van git pull origin master')
 	return HttpResponse("")
