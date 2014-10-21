476aafd0a5cf6e4b685cb0f92deaf42be362f61f
vanetssa <vanetssa@nate.com>
2014-10-21 17:48:04 +0900

    hook 수정

--- van/views.py
+++ van/views.py
@@ -5,5 +5,5 @@ def index(request):
 	return HttpResponse("hello world!!")
 
 def pull(request):
-	os.system('cd /website/van && sudo -u van git reset --hard HEAD && sudo -u van git pull origin mas')
+	os.system('cd /website/van && sudo -u van git reset --hard HEAD && sudo -u van git pull origin master')
 	return HttpResponse("")
