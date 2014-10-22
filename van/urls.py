41dd1f259d73d4ac7b39f3a13114a2363be1bc01
김상은 <whitesandfox@gmail.com>
2014-10-22 11:48:15 +0900

    blog 추가

--- van/urls.py
+++ van/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns,url
-from van.views import index
+from van.views import index,blog
 
 urlpatterns = patterns('',
     # url(r'^$', 'van.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$',index)
+	url(r'^$',index),
+    url(r'^blog$',blog)
 )
--- van/views.py
+++ van/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("hello world!!")
+
+def blog(reques):
+	return HttpResponse("blog page")
