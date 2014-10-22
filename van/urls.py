609fc1b71b56b80930397aff1c7b5114fb3cc296
김상은 <whitesandfox@gmail.com>
2014-10-22 13:43:03 +0900

    study 추가

--- van/urls.py
+++ van/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns,url
-from van.views import index,blog,community
+from van.views import index,study,blog,community
 
 urlpatterns = patterns('',
     # url(r'^$', 'van.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 	url(r'^$',index),
+    url(r'^study$',study),
     url(r'^blog$',blog),
     url(r'^community$',community)
 )
--- van/views.py
+++ van/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("hello world!!")
 
+def study(reques):
+	return HttpResponse("study page")
+
 def blog(reques):
 	return HttpResponse("blog page")
 
